6d3826b38b6f08e6e546bbfcf74d9ea585258e32
Ilya Andreev <mrbrikster@yandex.ru>
2021-11-15 15:30:28 +0300

    Fix some issues

--- sshfs-mount.sh
+++ sshfs-mount.sh
@@ -5,12 +5,15 @@ SSHFS_USER_DIR="$HOME/SSHFS"
 mkdir -p "$SSHFS_USER_DIR"
 
 if [ ! -z "$1" ]; then
-    if [ ! -d "$SSHFS_USER_DIR/$1" ]; then
-        mkdir -p "$SSHFS_USER_DIR/$1"
+    LOCAL_MOUNT_PATH="$SSHFS_USER_DIR/$1"
+
+    if ! mount | grep -q "${LOCAL_MOUNT_PATH} "
+    then
+        mkdir -p $LOCAL_MOUNT_PATH
 
         MOUNT_POINT="/home"
 
-        if [ ! -z "$2" ]; then 
+        if [ ! -z "$2" ]; then
             MOUNT_POINT="$2"
         fi
 
@@ -20,10 +23,16 @@ if [ ! -z "$1" ]; then
             -o defer_permissions \
             -o jail_symlinks \
             -o noappledouble \
-            $1:$MOUNT_POINT $SSHFS_USER_DIR/$1 
+            $1:$MOUNT_POINT $LOCAL_MOUNT_PATH
 
-        echo "Host \"$1\" successfully mount with point \"$MOUNT_POINT\"."
-        exit 0
+        if [ $? -eq 0 ];
+        then
+            echo "Host \"$1\" successfully mount with point \"$MOUNT_POINT\"."
+            exit 0
+        else
+            echo "Error when mounting host \"$1\"."
+            exit 1;
+        fi
     else
         echo "Host \"$1\" is already mounted."
         exit 1
